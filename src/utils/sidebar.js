35633d1f7a6c6db9ebb4792c5be02013956a19d4
AdrifeCova <adrife2000@hotmail.com>
2024-02-15 08:42:35 -0600

    add new feature in the navbar

--- src/utils/sidebar.js
+++ src/utils/sidebar.js
@@ -3,8 +3,9 @@ const sidebar__toggle = document.querySelector(".sidebar__toggle");
 const sidebar = document.querySelector(".sidebar");
 const bg__opacity = document.querySelector(".opacity-background");
 const hero = document.querySelector(".hero");
+const navbar = document.querySelector("header");
 
-navbar__menu.addEventListener("click", () => {
+navbar__menu.addEventListener("click", (e) => {
   sidebar.classList.add("open");
   bg__opacity.classList.add("open");
   hero.classList.add("opacity");
@@ -22,4 +23,31 @@ sidebar__toggle.addEventListener("click", () => {
   sidebar.classList.remove("open");
   bg__opacity.classList.remove("open");
   hero.classList.remove("opacity");
+});
+
+navbar__menu.addEventListener("keydown", (e) => {
+  // Key
+  // if (e.key === "Enter") {
+  //   alert("1");
+  // }
+
+  // KeyCode
+  // if (e.keyCode === 13) {
+  //   alert(1);
+  // }
+
+  // code
+  if (e.code === "Escape") {
+    sidebar.classList.remove("open");
+    bg__opacity.classList.remove("open");
+    hero.classList.remove("opacity");
+  }
+});
+
+document.addEventListener("wheel", (e) => {
+  if (e.deltaY === -100) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
 });
