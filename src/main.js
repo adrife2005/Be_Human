ad18e9806db09e2ef50bf7393b69b6e1078a6fad
AdrifeCova <adrife2000@hotmail.com>
2024-06-05 14:45:36 -0600

    the last changes

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import '../css/components/equipment.css';
 import '../css/components/staff.css';
 import '../css/components/location.css';
 import '../css/components/enroll.css';
-import '../css/components/footer.css';
+import '../css//pie.css';
 import '../css/utils.css';
 
 import './utils/sidebar';
